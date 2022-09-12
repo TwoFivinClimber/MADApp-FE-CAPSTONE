c3d946dd4f6dae4440bc4da3fa49b503f8cba0ff
Brett Hughes <twofivinclimber@gmail.com>
2022-09-11 22:22:10 -0500

    fixed error

--- api/tom-tom.js
+++ api/tom-tom.js
@@ -8,9 +8,22 @@ const tomApi = 'h7BrcblaIvpXTTQtlrux4NjI1EMNNTnk';
 // FUZZY EXAMPLE
 // https://api.tomtom.com/search/2/search/36.98844,-121.97483.json?key={Your_API_Key}
 
-const getPoi = () => new Promise((resolve, reject) => {
-  axios.get(`https://api.tomtom.com/search/2/search/Brooklyn%20Bowl.json?countrySet=US&lat=36.188806&lon=-86.762462&language=en-US&extendedPostalCodesFor=POI&minFuzzyLevel=1&maxFuzzyLevel=1&idxSet=POI&view=Unified&relatedPois=off&key=${tomApi}`)
-    .then((result) => resolve(Object.values(result.data.results)))
+const getPoi = (input) => new Promise((resolve, reject) => {
+  axios.get(`https://api.tomtom.com/search/2/search/${input}.json?countrySet=US&lat=36.188806&lon=-86.762462&language=en-US&extendedPostalCodesFor=POI&minFuzzyLevel=1&maxFuzzyLevel=2&idxSet=POI&view=Unified&relatedPois=off&key=${tomApi}`)
+    .then((result) => {
+      const poiArray = Object.values(result.data.results);
+      // console.warn(poiArray);
+      const returnArray = poiArray.map((poi) => (
+        {
+          value: poi.poi.name,
+          label: poi.poi.name,
+          city: poi.address.localName,
+          state: poi.address.countrySubdivision,
+          name: 'location',
+        }
+      ));
+      resolve(returnArray);
+    })
     .catch(reject);
 });
 
--- pages/tom-tom.js
+++ pages/tom-tom.js
@@ -1,25 +1,38 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import AsyncSelect from 'react-select/async';
 import getPoi from '../api/tom-tom';
 
 function tomTom() {
-  const [results, setResults] = useState([]);
-  const testTom = () => {
-    getPoi().then(setResults);
-  };
+  // const [results, setResults] = useState([]);
+  // const [options, setOptions] = useState([]);
+  // const testTom = () => {
+  //   getPoi().then(setResults);
+  // };
 
-  useEffect(() => {
-    testTom();
-  }, []);
+  // useEffect(() => {
+  //   testTom();
+  // }, []);
+
+  const promiseOptions = (inputValue) => new Promise((resolve, reject) => {
+    getPoi(inputValue).then((result) => {
+      resolve(result);
+    }).catch(reject);
+  });
+
+  const handleSelect = (value) => {
+    console.warn(value);
+  };
 
   return (
     <div>
       <h4>Results</h4>
-      <ul>
+      {/* <ul>
         {results.map((result) => (
           <li>{result.poi.name}</li>
         ))}
-      </ul>
+      </ul> */}
+      <AsyncSelect isClearable onChange={handleSelect} loadOptions={promiseOptions} />
     </div>
   );
 }
