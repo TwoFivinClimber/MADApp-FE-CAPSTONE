e520e77a04860218dd33871f751d297d05df4d53
Brett Hughes <twofivinclimber@gmail.com>
2022-10-08 22:47:36 -0500

    removed console.warn

--- api/tom-tom.js
+++ api/tom-tom.js
@@ -7,7 +7,6 @@ const getPoi = (input, lat, long) => new Promise((resolve, reject) => {
   axios.get(`https://api.tomtom.com/search/2/search/${input}.json?&lat=${lat}&lon=${long}&language=en-US&extendedPostalCodesFor=POI&minFuzzyLevel=1&maxFuzzyLevel=2&idxSet=POI&view=Unified&relatedPois=off&key=h7BrcblaIvpXTTQtlrux4NjI1EMNNTnk`)
     .then((result) => {
       const poiArray = Object.values(result.data.results);
-      console.warn(poiArray);
       const returnArray = poiArray.map((poi) => (
         {
           value: poi.poi.name,
