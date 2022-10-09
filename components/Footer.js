7cf28ab312b84cf44c56577082ceb2cbd0a74c33
Brett Hughes <twofivinclimber@gmail.com>
2022-10-08 23:06:59 -0500

    fixed minor nav stuff and footer

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
           <Nav.Link href="/">Home</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <h6>MAD App Copyright</h6>
+          <Nav.Link>MAD App Copyright</Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link href="https://www.linkedin.com/in/brett-hughes-08942893">Connect</Nav.Link>
